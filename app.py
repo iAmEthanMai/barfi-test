f753766633b0fb4f3639ddef36c26f1e4ac8bb70
Ethan Mai <57975262+iAmEthanMai@users.noreply.github.com>
2023-02-24 16:19:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,9 @@ from barfi import Block
 
 
 add = Block(name='Addition')
+add.add_input(name='my input')
 sub = Block(name='Subtraction')
 mul = Block(name='Multiplication')
 div = Block(name='Division')
 
-barfi_result = st_barfi(base_blocks= [add, sub, mul, div])
-# or if you want to use a category to organise them in the frontend sub-menu
 barfi_result = st_barfi(base_blocks= {'Op 1': [add, sub], 'Op 2': [mul, div]})
